01f463d802f7fbc5211944accd6e4d2a6ad76d6b
SammeJanderson <samme.san@gmail.com>
2021-02-10 11:27:44 -0300

    project setup

---
+++ src/main/java/com/github/sammejanderson/StudyAssistApp/StudyAssistAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.sammejanderson.StudyAssistApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAssistAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAssistAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sammejanderson/StudyAssistApp/StudyAssistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sammejanderson.StudyAssistApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAssistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
