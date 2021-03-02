fe6da565657ab75d086e5aa1105634c528039870
SammeJanderson <samme.san@gmail.com>
2021-03-02 12:58:59 -0300

    configurations

--- src/main/java/com/github/sammejanderson/StudyAssistApp/exception/CardNotFoundException.java
+++ src/main/java/com/github/sammejanderson/StudyAssistApp/exception/CardNotFoundException.java
@@ -1,5 +1,9 @@
 package com.github.sammejanderson.StudyAssistApp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CardNotFoundException extends Exception {
     public CardNotFoundException(long id){
         super ("Card with Id: "+ id + " not found");
--- src/main/java/com/github/sammejanderson/StudyAssistApp/exception/ContainerUpdateException.java
+++ src/main/java/com/github/sammejanderson/StudyAssistApp/exception/ContainerUpdateException.java
@@ -1,5 +1,9 @@
 package com.github.sammejanderson.StudyAssistApp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
 public class ContainerUpdateException extends Exception {
     public ContainerUpdateException(Long id) {
         super("Card with Id: " + id + "it's already in the day container");
